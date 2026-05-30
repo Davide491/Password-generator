802ee0f9cd3bd519b71dda288b2cc37128004566
davidemaconi476-hash <davidemaconi476@gmail.com>
2026-05-30 13:31:26 +0200

    A password generatore totaly ofline and secure in GPLv3 license

---
+++ generator.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"math/big"
+)
+
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+
+func main() {
+	length := flag.Int("length", 12, "Lunghezza della password")
+	flag.Parse()
+	password, err := generatePassword(*length)
+	if err != nil {
+		fmt.Println("Errore nella generazione della password:", err)
+		return
+	}
+	fmt.Println(password)
+}
+
+func generatePassword(length int) (string, error) {
+	b := make([]byte, length)
+	charsetLength := big.NewInt(int64(len(characters)))
+
+	for i := range b {
+		n, err := rand.Int(rand.Reader, charsetLength)
+		if err != nil {
+			return "", err
+		}
+		b[i] = characters[n.Int64()]
+	}
+
+	return string(b), nil
+}
